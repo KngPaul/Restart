a75e02087e0492f87f3d43a0a8821818bf96f23f
Paul Onawola <onawola.ire@gmail.com>
2024-05-13 17:15:40 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Paul Onawola on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
